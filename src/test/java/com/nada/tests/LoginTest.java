b2a9471d38cb78e68e95fb2052d6ddb7a13faf0b
nada <nadaelfadil@gmail.com>
2026-09-25 17:23:19 +0000

    Change on LoginTest

--- src/test/java/com/nada/tests/LoginTest.java
+++ src/test/java/com/nada/tests/LoginTest.java
@@ -10,44 +10,5 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LoginTest {
 
-    private WebDriver driver;
 
-    @BeforeEach
-    public void setUp(){
-        driver = new ChromeDriver();
-    }
-
-    @AfterEach
-    public void tearDown(){
-        driver.quit();
-    }
-
-      @Test
-      @Tag("smoke")
-    public void loginTestwithValidCredentialsShouldRedirectToOverviewAccounts(){
-        LoginPage loginPage = new LoginPage(driver);
-        loginPage.open();
-        loginPage.enterUsername("nada");
-        loginPage.enterPassword("password");
-        loginPage.clickOnLogin();
-//        loginPage.login("nada","password");
-        Assertions.assertEquals("ParaBank | Accounts Overview", driver.getTitle());
-
-      }
-
-
-      @ParameterizedTest
-      @Tag("regression")
-      @CsvSource({
-              "nada, wrong", "wrong, password"
-      })
-    public void loginWithFalseCredentialsShouldShowErrorPage(String username, String password){
-          LoginPage loginPage = new LoginPage(driver);
-          loginPage.open();
-          loginPage.enterUsername(username);
-          loginPage.enterPassword(password);
-          loginPage.clickOnLogin();
-//          loginPage.login(username,password);
-          Assertions.assertEquals("The username and password could not be verified.",loginPage.getErrorMessage());
-      }
 }
